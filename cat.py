2e4d9921182315b9c2d96ceae38274e9d1384c3c
swifty-hh <106498918+swifty-hh@users.noreply.github.com>
2023-09-25 21:43:09 +0900

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,4 +1,12 @@
-i = 3
-while i != 0:
-    print("meow!")
-    i = i - 1
+#i = 3
+#while i != 0:
+#    print("meow!")
+#    i = i - 1
+
+#for i in [0, 1, 2]: 
+#    print("Meow")
+
+#for i in range(3): # for i in range = loop method
+#    print("meow")
+
+print("meow\n" * 3, end="") # \n = "enter" / end="" = makes ending after ""
