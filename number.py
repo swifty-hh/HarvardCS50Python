9af3b369f3eb699d136f7106876c0b9af3cdee97
swifty-hh <106498918+swifty-hh@users.noreply.github.com>
2023-10-04 21:16:48 +0900

    Update number.py

--- number.py
+++ number.py
@@ -1,9 +1,13 @@
-while True:
-    try:
-        x = int(input("What is x? "))
-    except ValueError:
-        print("x is not an integer.")
-    else:
-        break
+def main():
+    x = get_int()
+    print(f"x is {x}.")
 
-print(f"x is {x}.")
+def get_int():
+    while True:
+        try:
+            return int(input("What is x? "))
+        except ValueError:
+            print("That is not an integer.") # 'pass' for same function but no guide
+            
+
+main()
